3b6e53a4da2448abde17e2f80ba0c2ea0c2a04b6
juaumkiko <joaohenriquesouza80@gmail.com>
2021-05-13 07:23:21 -0300

    Exemplo de documentacao Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+
+import swagger from "swagger-ui-express";
 import { app } from ".";
 
+import swaggerFile from "./docs/swagger";
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile()));
 app.listen(3333, () => console.log("Server is running!"));
